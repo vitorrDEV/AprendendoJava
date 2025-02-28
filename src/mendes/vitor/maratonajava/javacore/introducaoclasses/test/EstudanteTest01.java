d62f9288ec9bc5f415f4011fed812217d7dbe0d4
vitorrDEV <vitorrmendes45@gmail.com>
2025-02-28 13:26:13 -0300

    dominio

---
+++ src/mendes/vitor/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,14 @@
+package mendes.vitor.maratonajava.javacore.introducaoclasses.test;
+
+import mendes.vitor.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+
+    }
+}
